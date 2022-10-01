b5f4915f9c40352906e94517d3a72e5dfd3d31f6
eunsukimme <eunsu.dev@gmail.com>
2022-10-01 16:15:31 +0900

    infra: add ec2 constructors

--- lib/uniview-ia_c-stack.ts
+++ lib/uniview-ia_c-stack.ts
@@ -27,55 +27,55 @@ export class UniviewIaCStack extends cdk.Stack {
     });
 
     // 👇 create a security group for the EC2 instance
-    // const ec2InstanceSG = new ec2.SecurityGroup(this, "uniview-instance-sg", {
-    //   vpc,
-    // });
+    const ec2InstanceSG = new ec2.SecurityGroup(this, "uniview-instance-sg", {
+      vpc,
+    });
 
-    // ec2InstanceSG.addIngressRule(
-    //   ec2.Peer.anyIpv4(),
-    //   ec2.Port.tcp(80),
-    //   "allow TCP connection on PORT 80 from anywhere(server port)"
-    // );
-    // ec2InstanceSG.addIngressRule(
-    //   ec2.Peer.anyIpv4(),
-    //   ec2.Port.tcp(5000),
-    //   "allow TCP connection on PORT 4000 from anywhere(server port)"
-    // );
+    ec2InstanceSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      "allow TCP connection on PORT 80 from anywhere(server port)"
+    );
+    ec2InstanceSG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(5000),
+      "allow TCP connection on PORT 4000 from anywhere(server port)"
+    );
 
-    // // create EBS root volume
-    // const rootVolume: ec2.BlockDevice = {
-    //   deviceName: "/dev/xvda", // Use the root device name
-    //   volume: ec2.BlockDeviceVolume.ebs(30), // Override the volume size in Gibibytes (GiB)
-    // };
-    // // 👇 create the EC2 instance
-    // const ec2Instance = new ec2.Instance(this, "uniview-instance", {
-    //   vpc,
-    //   vpcSubnets: {
-    //     subnetType: ec2.SubnetType.PUBLIC,
-    //   },
-    //   securityGroup: ec2InstanceSG,
-    //   instanceType: ec2.InstanceType.of(
-    //     ec2.InstanceClass.BURSTABLE2,
-    //     ec2.InstanceSize.MICRO
-    //   ),
-    //   machineImage: new ec2.GenericLinuxImage({
-    //     // NOTE: AMI from uniview-backend-1
-    //     "ap-northeast-2": "ami-045fe7351ba18fc80",
-    //   }),
-    //   blockDevices: [rootVolume],
-    // });
-    // ec2Instance.role.addManagedPolicy(
-    //   iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
-    // );
+    // create EBS root volume
+    const rootVolume: ec2.BlockDevice = {
+      deviceName: "/dev/xvda", // Use the root device name
+      volume: ec2.BlockDeviceVolume.ebs(30), // Override the volume size in Gibibytes (GiB)
+    };
+    // 👇 create the EC2 instance
+    const ec2Instance = new ec2.Instance(this, "uniview-instance", {
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      securityGroup: ec2InstanceSG,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE2,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: new ec2.GenericLinuxImage({
+        // NOTE: AMI from uniview-backend-1
+        "ap-northeast-2": "ami-045fe7351ba18fc80",
+      }),
+      blockDevices: [rootVolume],
+    });
+    ec2Instance.role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
+    );
 
-    // // elastic IP
-    // const eip = new ec2.CfnEIP(this, "server-ip");
-    // // EC2 Instance <> EIP
-    // // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    // const eipAssociation = new ec2.CfnEIPAssociation(this, "Ec2Association", {
-    //   eip: eip.ref,
-    //   instanceId: ec2Instance.instanceId,
-    // });
+    // elastic IP
+    const eip = new ec2.CfnEIP(this, "server-ip");
+    // EC2 Instance <> EIP
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const eipAssociation = new ec2.CfnEIPAssociation(this, "Ec2Association", {
+      eip: eip.ref,
+      instanceId: ec2Instance.instanceId,
+    });
 
     // 👇 create RDS instance
     const dbInstance = new rds.DatabaseInstanceFromSnapshot(
@@ -107,7 +107,7 @@ export class UniviewIaCStack extends cdk.Stack {
         credentials: rds.SnapshotCredentials.fromGeneratedSecret("postgres"),
       }
     );
-    // dbInstance.connections.allowFrom(ec2Instance, ec2.Port.tcp(5432));
+    dbInstance.connections.allowFrom(ec2Instance, ec2.Port.tcp(5432));
 
     new cdk.CfnOutput(this, "dbEndpoint", {
       value: dbInstance.instanceEndpoint.hostname,
