842fb1f138362a312725bab71301d164bde8f9e2
eunsukimme <eunsu.dev@gmail.com>
2022-10-01 15:45:02 +0900

    infra: add stack

--- lib/uniview-ia_c-stack.ts
+++ lib/uniview-ia_c-stack.ts
@@ -1,16 +1,120 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as rds from "aws-cdk-lib/aws-rds";
 
-export class UniviewIaCStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class UniviewIaCStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 👇 create the VPC
+    const vpc = new ec2.Vpc(this, "uniview-vpc", {
+      cidr: "10.0.0.0/16",
+      natGateways: 0,
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          name: "public-subnet-1",
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: "isolated-subnet-1",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 28,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'UniviewIaCQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // 👇 create a security group for the EC2 instance
+    // const ec2InstanceSG = new ec2.SecurityGroup(this, "uniview-instance-sg", {
+    //   vpc,
     // });
+
+    // ec2InstanceSG.addIngressRule(
+    //   ec2.Peer.anyIpv4(),
+    //   ec2.Port.tcp(80),
+    //   "allow TCP connection on PORT 80 from anywhere(server port)"
+    // );
+    // ec2InstanceSG.addIngressRule(
+    //   ec2.Peer.anyIpv4(),
+    //   ec2.Port.tcp(5000),
+    //   "allow TCP connection on PORT 4000 from anywhere(server port)"
+    // );
+
+    // // create EBS root volume
+    // const rootVolume: ec2.BlockDevice = {
+    //   deviceName: "/dev/xvda", // Use the root device name
+    //   volume: ec2.BlockDeviceVolume.ebs(30), // Override the volume size in Gibibytes (GiB)
+    // };
+    // // 👇 create the EC2 instance
+    // const ec2Instance = new ec2.Instance(this, "uniview-instance", {
+    //   vpc,
+    //   vpcSubnets: {
+    //     subnetType: ec2.SubnetType.PUBLIC,
+    //   },
+    //   securityGroup: ec2InstanceSG,
+    //   instanceType: ec2.InstanceType.of(
+    //     ec2.InstanceClass.BURSTABLE2,
+    //     ec2.InstanceSize.MICRO
+    //   ),
+    //   machineImage: new ec2.GenericLinuxImage({
+    //     // NOTE: AMI from uniview-backend-1
+    //     "ap-northeast-2": "ami-045fe7351ba18fc80",
+    //   }),
+    //   blockDevices: [rootVolume],
+    // });
+    // ec2Instance.role.addManagedPolicy(
+    //   iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
+    // );
+
+    // // elastic IP
+    // const eip = new ec2.CfnEIP(this, "server-ip");
+    // // EC2 Instance <> EIP
+    // // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // const eipAssociation = new ec2.CfnEIPAssociation(this, "Ec2Association", {
+    //   eip: eip.ref,
+    //   instanceId: ec2Instance.instanceId,
+    // });
+
+    // 👇 create RDS instance
+    const dbInstance = new rds.DatabaseInstanceFromSnapshot(
+      this,
+      "uniview-db-instance",
+      {
+        engine: rds.DatabaseInstanceEngine.postgres({
+          version: rds.PostgresEngineVersion.VER_12,
+        }),
+        snapshotIdentifier:
+          "arn:aws:rds:ap-northeast-2:351990402606:snapshot:uniview-db-20221001",
+        vpc,
+        vpcSubnets: {
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+        instanceType: ec2.InstanceType.of(
+          ec2.InstanceClass.BURSTABLE2,
+          ec2.InstanceSize.MICRO
+        ),
+        allowMajorVersionUpgrade: false,
+        autoMinorVersionUpgrade: true,
+        deleteAutomatedBackups: true,
+        publiclyAccessible: false,
+        deletionProtection: false,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+        backupRetention: cdk.Duration.days(0),
+        allocatedStorage: 20,
+        multiAz: false,
+        credentials: rds.SnapshotCredentials.fromGeneratedSecret("postgres"),
+      }
+    );
+    // dbInstance.connections.allowFrom(ec2Instance, ec2.Port.tcp(5432));
+
+    new cdk.CfnOutput(this, "dbEndpoint", {
+      value: dbInstance.instanceEndpoint.hostname,
+    });
+
+    new cdk.CfnOutput(this, "secretName", {
+      value: dbInstance.secret?.secretName as string,
+    });
   }
 }
